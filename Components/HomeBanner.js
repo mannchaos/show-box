812b7dfd63886ab76fb1ea3c3b8966c8b151f699
mannchaos <mann@jetbro.in>
2023-02-25 13:58:06 +0530

    update

--- Components/HomeBanner.js
+++ Components/HomeBanner.js
@@ -12,8 +12,18 @@ export const HomeBanner = () => {
 					sizes="(max-width : 1920px) 100%,
 				(height: 100%)"
 				/>
-				<div>
-					<p className={HomeBannerStyle.movie__title}>La La Land</p>
+				<div className={HomeBannerStyle.movie__info}>
+					<p className={HomeBannerStyle.movie__info__title}>La La Land</p>
+					<div className={HomeBannerStyle.movie__info__details}>
+						<p>Romance, Drama</p>
+						<div>
+							<Image src="/images/calendar.png" width={16} height={16} alt="calender-icon" />
+							<p>2019</p>
+						</div>
+					</div>
+					<div className={HomeBannerStyle.movie__info__type}>
+						<p>Tv Show</p>
+					</div>
 				</div>
 			</div>
 		</>
